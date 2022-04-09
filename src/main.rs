db7b7f2086ab6bc9dc277950a8b1d8288e8934fd
Sabr <sbrsuleiman@gmail.com>
2022-04-08 22:35:42 -0400

    added db to server

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,23 @@
 use actix_web::{HttpServer, App};
+use sqlx::postgres::PgPoolOptions;
 
+#[allow(deprecated)]
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    dotenv::dotenv().ok();
+    let db_url = std::env::var("DATABASE_URL").expect("Unable to read DATABASE_URL variable in .env file!");
+    let pool = PgPoolOptions::new()
+        .max_connections(10)
+        .connect(&db_url)
+        .await
+        .expect("Unable to create database pool");
+
     HttpServer::new(move || {
+        let cors = actix_cors::Cors::permissive();
+
         App::new()
+            .wrap(cors)
+            .data(pool.clone())
     })
         .bind("127.0.0.1:8080")?
         .run()
