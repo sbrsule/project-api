7ba876381799bba40e77769ae442bf351b6fed6f
Sol Invictus <invictus@Sabrs-Air.attlocal.net>
2022-04-13 23:08:09 -0400

    created post model

--- src/models/mod.rs
+++ src/models/mod.rs
@@ -1,2 +1,3 @@
 pub mod user;
 pub mod password;
+pub mod post;
---
+++ src/models/post.rs
@@ -0,0 +1,62 @@
+use anyhow::Result;
+use serde::{Serialize, Deserialize};
+use sqlx::{PgPool, postgres::{PgRow, PgQueryResult}, Row};
+
+#[derive(Serialize, Deserialize)]
+pub struct PostRequest {
+    pub body: String,
+    pub user_id: i32,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct Post {
+    pub id: i32,
+    pub user_id: i32,
+    pub body: String,
+    pub created: chrono::NaiveDateTime,
+    pub reply_id: Vec<i32>,
+}
+
+impl Post {
+    pub async fn find_top_ten(pool: &PgPool) -> Result<Vec<Post>> {
+        let posts: Vec<Post> = sqlx::query_as!(
+            Post,
+            r#"
+                SELECT DISTINCT * FROM posts
+                ORDER BY created
+                FETCH FIRST 10 ROWS ONLY 
+            "#
+        )
+            .fetch_all(pool)
+            .await?;
+
+        Ok(posts)
+    }
+
+    pub async fn create_post(post: PostRequest, pool: &PgPool) -> Result<PgQueryResult> {
+        let mut table = pool.begin().await?;
+        let created = sqlx::query("
+            INSERT INTO posts (body, user_id) values ($1, $2) RETURNING *
+        ")
+            .bind(post.body)
+            .bind(post.user_id)
+            .execute(&mut table)
+            .await?;
+
+        table.commit().await?;
+        Ok(created)
+    }
+
+    pub async fn delete_post(post_id: i32, pool: &PgPool) -> Result<PgQueryResult> {
+        let mut table = pool.begin().await?;
+        let deleted = sqlx::query("
+            DELETE FROM posts WHERE id = $1
+        ")
+            .bind(post_id)
+            .execute(&mut table)
+            .await?;
+
+        table.commit().await?;
+        Ok(deleted)
+    }
+}
