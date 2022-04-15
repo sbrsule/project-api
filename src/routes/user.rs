99331b827583666a19307ad9684b5e781a58dbc6
Sol Invictus <invictus@Sabrs-Air.attlocal.net>
2022-04-14 20:19:53 -0400

    added logout route

--- src/routes/user.rs
+++ src/routes/user.rs
@@ -1,5 +1,5 @@
 use actix_identity::Identity;
-use actix_web::{post, web, HttpResponse};
+use actix_web::{post, web, HttpResponse, get};
 use sqlx::PgPool;
 
 use crate::models::user::{User, UserRequest};
@@ -19,11 +19,12 @@ async fn register_user(id: Identity, user: web::Json<UserRequest>, pool: web::Da
 async fn login_user(id: Identity, user: web::Json<UserRequest>, pool: web::Data<PgPool>) -> HttpResponse {
     let username = user.username.clone();
     let password = user.password.clone();
+    let user_id = User::get_id(username, pool.get_ref()).await.expect("Could'nt find user id");
     match User::get_password(user.into_inner(), pool.get_ref()).await {
         Ok(password_hash) => {
             match crate::models::password::verify_password(password, password_hash) {
                 true => {
-                    id.remember(username);
+                    id.remember(user_id.to_string());
                     HttpResponse::Accepted().finish()
                 }
                 false => HttpResponse::Unauthorized().finish()
@@ -33,7 +34,30 @@ async fn login_user(id: Identity, user: web::Json<UserRequest>, pool: web::Data<
     }
 }
 
+#[post("/test")]
+async fn test(id: Identity) -> HttpResponse {
+    match id.identity() {
+        Some(_) => HttpResponse::Ok().finish(),
+        None => HttpResponse::Forbidden().finish(),
+    }
+}
+
+#[post("/logout")]
+async fn logout_user(id: Identity) -> HttpResponse {
+    match id.identity() {
+        Some(_) => {
+            id.forget();
+            HttpResponse::Ok().finish()
+        }
+        None => HttpResponse::NotFound().finish()
+    }
+}
+
+
+
 pub fn init(cfg: &mut web::ServiceConfig) {
     cfg.service(register_user);
     cfg.service(login_user);
+    cfg.service(test);
+    cfg.service(logout_user);
 }
